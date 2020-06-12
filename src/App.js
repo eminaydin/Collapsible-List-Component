8c4d712b2ad65b8491ead524801c2d35ee858138
eminaydin <eminaydin93@yahoo.com>
2020-06-12 22:52:32 +0200

    Typo fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import Collapsible from "./components/collapsible";
+import Collapsible from "./components/index";
 import data from "./components/collapsible/data.json"
 
 function App() {
-  return ( <div className="container"> 
-  <h1>Collapsible List</h1>
-  <ul className="collapse" >
-    {data.map(data=> {
-      return <Collapsible data = {data} key={data.id}/>
-    })}
-  
-  </ul>
-  
-  </div>
+  return (
+    <div className="container">
+      <h1>Collapsible List</h1>
+      <ul className="collapse" >
+        {data.map(data => {
+          return <Collapsible data={data} key={data.id} />
+        })}
+
+      </ul>
+
+    </div>
   );
 }
 
